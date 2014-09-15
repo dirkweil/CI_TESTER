2273208f5523094853f2ec5b8db5687826df25a6
Dirk Weil <dirk.weil@gedoplan.de>
2014-09-15 18:01:50 +0200

    Fix Umlaut-Zählung

--- src/main/java/de/gedoplan/seminar/citester/StringUtil.java
+++ src/main/java/de/gedoplan/seminar/citester/StringUtil.java
@@ -5,14 +5,14 @@ import java.text.StringCharacterIterator;
 
 /**
  * Utility-Klasse mit einigen (unsinnigen) String-Funktionen.
- * 
+ *
  * @author dw
  */
 public final class StringUtil
 {
   /**
    * Zählt die KLeinbuchstaben in einem String.
-   * 
+   *
    * @param s String
    * @return Anzahl Kleinbuchstaben
    */
@@ -22,8 +22,8 @@ public final class StringUtil
     CharacterIterator characterIterator = new StringCharacterIterator(s);
     for (char ch = characterIterator.first(); ch != CharacterIterator.DONE; ch = characterIterator.next())
     {
-      if (ch >= 'a' && ch <= 'z') // [Fehler: Das klappt nur bei ASCII!]
-      //      if (Character.isLowerCase(ch))
+      // if (ch >= 'a' && ch <= 'z') // [Fehler: Das klappt nur bei ASCII!]
+      if (Character.isLowerCase(ch))
       {
         ++result;
       }
