56c9636a5c2ae0767437141af4ca46efe42bdf8a
Dirk Weil <dirk.weil@gedoplan.de>
2014-09-17 07:47:30 +0200

    .

--- src/main/java/de/gedoplan/seminar/citester/Fibonacci.java
+++ src/main/java/de/gedoplan/seminar/citester/Fibonacci.java
@@ -10,9 +10,9 @@ public final class Fibonacci
   /*
    * [CS-Warnung: Dies sollte Javadoc sein, ist aber nur ein einfacher Kommentar]
    * Ausgabe der Fibonacci-Folge bis 50
-   *
+   * 
    * @param n Stelle
-   *
+   * 
    * @return Wert
    */
   public static int fibonacci(int n)
--- src/main/java/de/gedoplan/seminar/citester/StringUtil.java
+++ src/main/java/de/gedoplan/seminar/citester/StringUtil.java
@@ -5,14 +5,14 @@ import java.text.StringCharacterIterator;
 
 /**
  * Utility-Klasse mit einigen (unsinnigen) String-Funktionen.
- *
+ * 
  * @author dw
  */
 public final class StringUtil
 {
   /**
    * Zählt die KLeinbuchstaben in einem String.
-   *
+   * 
    * @param s String
    * @return Anzahl Kleinbuchstaben
    */
@@ -23,7 +23,7 @@ public final class StringUtil
     for (char ch = characterIterator.first(); ch != CharacterIterator.DONE; ch = characterIterator.next())
     {
       if (ch >= 'a' && ch <= 'z') // [Fehler: Das klappt nur bei ASCII!]
-      // if (Character.isLowerCase(ch))
+      //      if (Character.isLowerCase(ch))
       {
         ++result;
       }
